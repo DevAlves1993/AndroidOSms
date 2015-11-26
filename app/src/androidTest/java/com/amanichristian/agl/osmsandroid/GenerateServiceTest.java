ae6cb352daaa95b7305bf68a6c4f708efcd1d280
Amani Christian Alves Cyrille <alvesamani@gmail.com>
2015-11-26 18:17:34 +0000

    Beginning TestUnit

---
+++ app/src/androidTest/java/com/amanichristian/agl/osmsandroid/GenerateServiceTest.java
@@ -0,0 +1,35 @@
+package com.amanichristian.agl.osmsandroid;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by AMANI CHRISTIAN CYRILLE on 26/11/2015.
+ */
+public class GenerateServiceTest extends TestCase
+{
+
+    public void testGeneratedToken() throws Exception
+    {
+
+    }
+
+    public void testSendSMS() throws Exception
+    {
+
+    }
+
+    public void testRemainderSMS() throws Exception
+    {
+
+    }
+
+    public void testStatisticSMS() throws Exception
+    {
+
+    }
+
+    public void testHistoricPurchase() throws Exception
+    {
+
+    }
+}
